7e6fa675e0022924e883862ebbb8cdd1c2b0e960
Marcelo <marcelomagario@gmail.com>
2022-09-20 20:41:22 +1000

    Example of concatenation, lenght of strings and string slice

---
+++ var.js
@@ -0,0 +1,14 @@
+
+// Concatenation
+
+const someAdjective = "Challenging";
+let myStr = "Learning to code is ";
+myStr += someAdjective
+
+// Find the Length of a String
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+lastNameLength = lastName.length;
+
+
