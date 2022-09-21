06a0f78f61562bbe4c61954d938783006f97555f
Marcelo <marcelomagario@gmail.com>
2022-09-21 17:33:44 +1000

    Create arrays in Javascript

---
+++ arrays.js
@@ -0,0 +1,2 @@
+// example of Array with strings and numbers
+const myArray = ["Banana", 2, "Orange", 3, 1, 8];
--- var.js
+++ var.js
@@ -8,7 +8,12 @@ myStr += someAdjective
 // Find the Length of a String
 let lastNameLength = 0;
 const lastName = "Lovelace";
-
 lastNameLength = lastName.length;
+
+// Grab the last letter of the var
+let lastNameLength = 0;
+const palavraChave = "Palavra"
+lastLetter = palavraChave.length[palavraChave.length - 1]
+
 
 
