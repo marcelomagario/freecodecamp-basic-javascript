20b38ae464b08a648c06a46448b95cfc9227f21d
Marcelo <marcelomagario@gmail.com>
2022-09-22 08:56:14 +1000

    Create conditionals in Javascript

---
+++ function.js
@@ -0,0 +1,24 @@
+// Write Reusable JavaScript with Functions
+function functionName() {
+    console.log("Hello World");
+  }
+
+functionName();
+
+//  Function with Arguments 2, 4
+
+function functionWithArgs(num1, num2) {
+    console.log(num1 + num2);
+  }
+  
+  functionWithArgs(2, 4);
+
+// another example
+
+function timesFive(num){
+    return num * 5;
+  }
+  
+  timesFive();
+
+  // Global Scope and Functions
